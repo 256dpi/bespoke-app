44f05aeb29632f52194ae96f33c33f878aa822e9
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-03-31 08:47:05 +0200

    added first tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
+var helpWindow = null;
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
@@ -11,10 +12,19 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+  console.log(electron.screen.getAllDisplays());
+
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+  helpWindow = new BrowserWindow({width: 800, height: 600});
+  helpWindow.loadURL('file://' + __dirname + '/index.html');
+
   mainWindow.on('closed', function() {
     mainWindow = null;
+  });
+
+  helpWindow.on('closed', function() {
+    helpWindow = null;
   });
 });
