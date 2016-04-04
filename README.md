8c423159d25d4d97fb83c55681448dcb4a2ee9db
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-04-04 16:19:55 +0200

    added simple solution

---
+++ README.md
@@ -0,0 +1,9 @@
+# bespoke-presenter
+
+## Caveats
+
+Using electron will make some libraries detect the environment as require.js.
+
+```jade
+script(src='https://code.jquery.com/jquery-2.2.2.min.js' onload="if(window.module) { window.$ = window.jQuery = window.module.exports; }")
+```
--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const BrowserWindow = electron.BrowserWindow;
 var appWindow = null;
 var presentationWindow = null;
 
+var installScript = `
+  function relay(id) {
+    const ipcRenderer = require('electron').ipcRenderer;
+    ipcRenderer.send('slide', id);
+  }
+
+  window.deck.on('next', function(event){
+    relay(event.index);
+  });
+
+  window.deck.on('prev', function(event){
+    relay(event.index);
+  });
+`;
+
+var controlScript = `
+  const ipcRenderer = require('electron').ipcRenderer;
+
+  ipcRenderer.on('slide', function(_, id) {
+    window.deck.slide(id);
+  });
+`;
+
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
     app.quit();
@@ -24,8 +47,20 @@ app.on('ready', function() {
   appWindow.loadURL('file://' + __dirname + '/index.html');
 
   appWindow.webContents.on('will-navigate', function(_, file){
-    startPresentation(file);
+    appWindow.webContents.once('dom-ready', function(){
+      appWindow.webContents.executeJavaScript(installScript);
+
+      startPresentation(file);
+    });
   });
+});
+
+electron.ipcMain.on('slide', function(_, id){
+  if(id != 0) {
+    id = id;
+  }
+
+  presentationWindow.webContents.send('slide', id);
 });
 
 function startPresentation(file) {
@@ -50,5 +85,9 @@ function startPresentation(file) {
     });
 
     presentationWindow.loadURL(file);
+
+    presentationWindow.webContents.once('dom-ready', function(){
+      presentationWindow.webContents.executeJavaScript(controlScript);
+    });
   }
 }
