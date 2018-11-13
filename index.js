92d8023ea8d6c74d4180c70172b3485ee056993e
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-11-13 10:42:38 +0100

    fix menu

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+const Menu = electron.Menu;
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const btoa = require('btoa');
@@ -22,6 +23,27 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+  const template = [{
+    label: 'Application',
+    submenu: [
+      { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); }}
+    ]}, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+    ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
   showStart();
 });
 
